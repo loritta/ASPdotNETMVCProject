a788149541f5fc9a6d026b07c1a700f1eb5617f8
DmKud <dmkudrik2003@gmail.com>
2018-06-23 23:19:02 -0400

    model&InitMigration

---
+++ ASPdotNETMVCProject/ASPdotNETMVCProject/Models/Garage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPdotNETMVCProject.Models
+{
+    public class Garage
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public List<Service>Services { get; set; }
+        public int ServiceID { get; set; }
+
+    }
+}
---
+++ ASPdotNETMVCProject/ASPdotNETMVCProject/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPdotNETMVCProject.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+
+        public List<Service> Services { get; set; }
+        public int ServiceID { get; set; }
+
+        public Customer Customer { get; set; }
+        public int CustomerID { get; set; }
+
+        public Garage Garage { get; set; }
+        public int GarageID { get; set; }
+    }
+}
