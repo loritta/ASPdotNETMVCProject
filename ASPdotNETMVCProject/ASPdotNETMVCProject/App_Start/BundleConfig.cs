e787b7acfb626a7581f997ef24dfaea07c84781c
Matthew <mpstinis@gmail.com>
2018-06-25 12:20:42 -0400

    Created "custom.css"
    Will add all new CSS to this file

--- ASPdotNETMVCProject/ASPdotNETMVCProject/App_Start/BundleConfig.cs
+++ ASPdotNETMVCProject/ASPdotNETMVCProject/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace ASPdotNETMVCProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
+                      
         }
     }
 }
